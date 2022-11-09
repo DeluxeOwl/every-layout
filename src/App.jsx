ee46c4045ba4657cfe5b4fba4d4c16f9f1ab66f6
DeluxeOwl <54327698+DeluxeOwl@users.noreply.github.com>
2022-11-09 23:39:36 +0200

    the box

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,18 @@ function App() {
           <p>Fourth element</p>
         </div>
       </div>
+      <h1>The box</h1>
+      <div>
+        <div className="stack">
+          <div className="box">Box 1</div>
+          <div className="box invert">Box 2</div>
+          <div className="box">Box 3</div>
+          <div className="box" style={{ padding: "0 !important" }}>
+            <div className="box invert">head</div>
+            <div className="box">body</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
