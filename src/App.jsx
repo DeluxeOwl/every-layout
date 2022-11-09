bf7c0928657ac10d11026f9e10efb6c780bac7b6
DeluxeOwl <54327698+DeluxeOwl@users.noreply.github.com>
2022-11-09 22:52:02 +0200

    the stack

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
-
-
+import "./App.css";
 function App() {
   return (
-    <div >
-      hello world
+    <div style={{ margin: "0 1rem 0 1rem" }}>
+      <h1>The stack</h1>
+      <div style={{ height: "268px" }}>
+        <div className="stack">
+          <p>First element</p>
+          <p>Second element</p>
+          <p>Third element</p>
+          <p>Fourth element</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
